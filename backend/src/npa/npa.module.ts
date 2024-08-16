e01f5bb75ebd53f3047cf7a4e238627b567fc91a
Nikhil <nikhils9802@gmail.com>
2024-08-17 00:39:30 +0530

    added backend code

--- backend/src/npa/npa.module.ts
+++ backend/src/npa/npa.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NpaService } from './npa.service';
 import { NpaController } from './npa.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [NpaController],
   providers: [NpaService],
 })
